07ad067889b6a39cd32048a637fa01ca74a62e9d
Gustavo Moreno <gmorenosmail@gmail.com>
2024-02-10 12:24:00 -0300

    GET Clients paged

--- src/main/java/com/devsuperior/desafio_crud_client/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio_crud_client/services/ClientService.java
@@ -4,9 +4,12 @@ import com.devsuperior.desafio_crud_client.dto.ClientDTO;
 import com.devsuperior.desafio_crud_client.entities.Client;
 import com.devsuperior.desafio_crud_client.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,7 +24,14 @@ public class ClientService {
         Client client = result.get();
         ClientDTO dto = new ClientDTO(client);
         return dto;
-
     }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+
 
 }
